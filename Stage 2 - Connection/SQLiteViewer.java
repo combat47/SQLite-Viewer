df44bbdd0a39e9b9694f87686704c8222e41afde
Combat47 <115223808+combat47@users.noreply.github.com>
2023-12-01 14:10:59 +0330

    SQLiteViewer.java

---
+++ Stage 2 - Connection/SQLiteViewer.java
@@ -0,0 +1,79 @@
+package viewer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class SQLiteViewer extends JFrame {
+
+    final String TITLE_OF_PROGRAM = "SQLite Viewer";
+    final String BTN_ENTER = "Open";
+    final String BTN_EXEC = "Execute";
+    DataBase dataBase;
+
+    public SQLiteViewer() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 800);
+        setLayout(null);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        JLabel nameLabel = new JLabel("Database: ");
+        nameLabel.setBounds(10, 7, 70, 30);
+        add(nameLabel);
+        JTextField fileNameTextField = new JTextField();
+        fileNameTextField.setName("FileNameTextField");
+        fileNameTextField.setBounds(75, 10, 505, 30);
+        add(fileNameTextField);
+
+        JButton openFileButton = new JButton(BTN_ENTER);
+        openFileButton.setName("OpenFileButton");
+        openFileButton.setBackground(Color.lightGray);
+        openFileButton.setBounds(585, 10, 80, 30);
+        add(openFileButton);
+
+        JLabel nameLabelCombo = new JLabel("Table: ");
+        nameLabelCombo.setBounds(10 , 47, 70, 30);
+        add(nameLabelCombo);
+        JComboBox<String> tablesComboBox = new JComboBox<>();
+        tablesComboBox.setName("TablesComboBox");
+        tablesComboBox.setBackground(Color.lightGray);
+        tablesComboBox.setBounds(75, 50, 590, 30);
+        add(tablesComboBox);
+
+        JLabel nameLabelQuery = new JLabel("Query: ");
+        nameLabelQuery.setBounds(10, 87, 70, 30);
+        add(nameLabelQuery);
+        JTextArea queryTextArea = new JTextArea();
+        queryTextArea.setName("QueryTextArea");
+        queryTextArea.setBounds(75, 90, 590, 90);
+        add(queryTextArea);
+
+        JButton executeQueryButton = new JButton(BTN_EXEC);
+        executeQueryButton.setName("ExecuteQueryButton");
+        executeQueryButton.setBackground(Color.lightGray);
+        executeQueryButton.setBounds(565, 190, 100, 30);
+        add(executeQueryButton);
+
+        openFileButton.addActionListener(e -> {
+            String fileName = fileNameTextField.getText();
+            if (!fileName.equals("")) {
+                System.out.println(fileName);
+                dataBase = new DataBase(fileName);
+                ArrayList<String> tables = dataBase.getTableNames();
+                tablesComboBox.removeAllItems();
+                tables.forEach(tablesComboBox::addItem);
+                queryTextArea.removeAll();
+            }
+        });
+
+        tablesComboBox.addActionListener(e -> {
+            String item = (String) tablesComboBox.getSelectedItem();
+            queryTextArea.removeAll();
+            queryTextArea.setText("SELECT * FROM " + item + ";");
+        });
+
+        setVisible(true);
+    }
+}
